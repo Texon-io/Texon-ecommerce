874ab85ce80ef0dc4b3a94f52892db58612bd1c1
ramez <ramez.elzoz74@gmail.com>
2026-01-04 17:13:59 +0200

    feat: handle active tap style in nav & add clear items to [wishlist, cart]

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,39 +1,53 @@
 import { CartItem } from "./CartItem";
 import { CartSummary } from "./CartSummary";
-import { useCart } from "./useCart";
+import { useCart, useCartActions } from "./useCart";
 import HandleEmptyState from "../../components/ui/HandleEmptyState";
 import { PageSkeletonLoading } from "@/components/ui/PageSkeletonLoading";
 import { PageError } from "@/components/ui/PageError";
+import ConfirmDeleteDialog from "@/components/ui/ConfirmDeleteDialog";
+import { useState } from "react";
+import { toast } from "sonner";
+import Button from "@/components/ui/Button";
 
 export default function Cart() {
   const { cartItems, totalItems, isLoading, error } = useCart();
+  const { clearCart } = useCartActions();
+  const [open, setOpen] = useState(false);
 
-  // if (error) return <PageError message={error.message} />;
-
-  // if (isLoading) {
-  //   return (
-  //     <section className="mx-auto max-w-7xl px-3 md:px-14 py-12">
-  //       <PageSkeletonLoading />
-  //     </section>
-  //   );
-  // }
-
-  // if (cartItems.length === 0) {
-  //   return (
-  //     <section className="mx-auto max-w-7xl px-3 md:px-14 py-12">
-  //       <HandleEmptyState page="cart" />
-  //     </section>
-  //   );
-  // }
   console.log(cartItems);
 
   return (
     <section className="mx-auto max-w-7xl px-3 md:px-14 py-12">
       {/* Header */}
-      <div className="mb-10">
+      <div
+        className="mb-8 flex flex-col md:flex-row gap-2
+                   items-start md:items-center justify-between"
+      >
         <h1 className="text-3xl font-semibold text-brand-black">
           Shopping Cart ({totalItems || "#"})
         </h1>
+        {totalItems > 0 && !isLoading && (
+          <Button
+            className="rounded-lg self-end"
+            size="sm"
+            variant="outline"
+            onClick={() => setOpen(true)}
+          >
+            Clear Cart
+          </Button>
+        )}
+        {/* Confirm Dialog */}
+        <ConfirmDeleteDialog
+          title="Remove all items?"
+          description="Are you sure you want to remove all items from your cart?"
+          open={open}
+          onOpenChange={setOpen}
+          onConfirm={() => {
+            clearCart();
+            setOpen(false);
+            toast.success("Removed from cart");
+          }}
+        />
       </div>
 
       {/* Loading state */}
